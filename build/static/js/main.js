bcec770127559eb48337d1070287c37063711324
Maryna Hapon <marynahapon@gmail.com>
2017-08-20 21:47:25 +0300

    module 'skills'
    - progessbar

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -21,3 +21,119 @@ $(document).ready(function() {
     });
 });
 
+
+    // progressbar
+var bar1 = new ProgressBar.Circle(webDesign, {
+    color: '#3c4761',
+    strokeWidth: 10,
+    trailWidth: 10,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 10 },
+    to: { color: '#30bae7', width: 10 },
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value + '%');
+        }
+
+    }
+});
+bar1.text.style.fontSize = '50px';
+bar1.animate(0.90);
+
+
+var bar2 = new ProgressBar.Circle(htmlCss, {
+    color: '#3c4761',
+    strokeWidth: 10,
+    trailWidth: 10,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 10 },
+    to: { color: '#d74680', width: 10 },
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value + '%');
+        }
+
+    }
+});
+bar2.text.style.fontSize = '50px';
+bar2.animate(0.75);
+
+
+var bar3 = new ProgressBar.Circle(graphicDesign, {
+    color: '#3c4761',
+    strokeWidth: 10,
+    trailWidth: 10,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 10 },
+    to: { color: '#17c2a4', width: 10 },
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value + '%');
+        }
+
+    }
+});
+bar3.text.style.fontSize = '50px';
+bar3.animate(0.70);
+
+
+var bar4 = new ProgressBar.Circle(uiUx, {
+    color: '#3c4761',
+    strokeWidth: 10,
+    trailWidth: 10,
+    easing: 'easeInOut',
+    duration: 1400,
+    text: {
+        autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 10 },
+    to: { color: '#eb7d4b', width: 10 },
+
+    step: function(state, circle) {
+        circle.path.setAttribute('stroke', state.color);
+        circle.path.setAttribute('stroke-width', state.width);
+
+        var value = Math.round(circle.value() * 100);
+        if (value === 0) {
+            circle.setText('');
+        } else {
+            circle.setText(value + '%');
+        }
+
+    }
+});
+bar4.text.style.fontSize = '50px';
+bar4.animate(0.85);
