cadc636982546ec7e6d9213cfa6809ad0cb3b6d8
Maryna Hapon <marynahapon@gmail.com>
2017-08-21 14:25:07 +0300

    module 'portfolio'
    - markup
    - JCON content
    - images added

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -143,4 +143,35 @@ $(document).ready(function() {
         }
     });
 
+
+    $(function () {
+        $(".portfolio-item").slice(0, 2).show();
+        $("#loadMore").on('click', function (e) {
+            e.preventDefault();
+            $("div:hidden").slice(0, 2).slideDown();
+            if ($("div:hidden").length == 0) {
+                $("#load").fadeOut('slow');
+            }
+            $('html,body').animate({
+                scrollTop: $(this).offset().top
+            }, 1500);
+        });
+    });
+
+    $('a[href=#top]').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 10) {
+            $('.totop a').fadeIn();
+        } else {
+            $('.totop a').fadeOut();
+        }
+    });
+
+
 });
